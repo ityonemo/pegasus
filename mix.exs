6f955046465d97fcef3ade6da47e7ed80e6ca3c8
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2024-12-13 10:53:33 -0600

    verup

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pegasus.MixProject do
   def project do
     [
       app: :pegasus,
-      version: "0.2.5",
+      version: "0.2.6",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
